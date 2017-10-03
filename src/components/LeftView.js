1327e1b28bf6c084bdbb1f6c0f8b26bad00c0fbe
Justice Appiah <jaxtis.apia@gmail.com>
2017-10-03 09:46:33 +0000

    update of pics

--- src/components/LeftView.js
+++ src/components/LeftView.js
@@ -1,11 +1,21 @@
 /**
  * Created by user on 2017-09-29.
  */
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import bg1 from '../img/bg1.jpg'
 import bg2 from '../img/bg2.jpg'
 import bg3 from '../img/bg3.jpg'
+import bg4 from '../img/bg4.jpg'
+import bg5 from '../img/bg5.jpg'
+import bg6 from '../img/bg6.jpg'
+import bg7 from '../img/bg7.jpg'
+import bg8 from '../img/bg8.jpg'
+import bg9 from '../img/bg9.jpg'
+import bg10 from '../img/bg10.jpg'
+import bg11 from '../img/bg11.jpg'
+import bg12 from '../img/bg12.jpg'
+import bg13 from '../img/bg13.jpg'
 
 // const image_urls = [
 //     "http://media.gettyimages.com/photos/african-girl-picture-id108312102",
@@ -13,49 +23,47 @@ import bg3 from '../img/bg3.jpg'
 // ];
 
 const image_urls = [
-    bg1,
-    bg2,
-    bg3
+    bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9, bg9, bg10, bg11, bg12, bg13
 ];
 
-export class LeftView extends Component{
+export class LeftView extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
-            image_index : 0,
-            image : image_urls[0]
+            image_index: 0,
+            image: image_urls[0]
         };
 
         this.changeBackground = this.changeBackground.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         setInterval(this.changeBackground, 5000);
     }
 
 
-    changeBackground(event){
+    changeBackground(event) {
 
-        if (this.state.image_index  === image_urls.length-1){
-            this.setState({image_index: 0})
+        if (this.state.image_index === image_urls.length - 1) {
+            this.setState({ image_index: 0 })
         }
         else {
-            this.setState({image_index: this.state.image_index + 1})
+            this.setState({ image_index: this.state.image_index + 1 })
         }
 
-    this.setState({
-        image:image_urls[this.state.image_index]
-    });
+        this.setState({
+            image: image_urls[this.state.image_index]
+        });
 
 
     }
 
-    render (){
+    render() {
 
         return (
-            <div className="split-image" style={{backgroundImage:`url(${this.state.image})`}}>
+            <div className="split-image" style={{ backgroundImage: `url(${this.state.image})` }}>
 
             </div>
         );
